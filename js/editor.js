70f4b3a4ad36d6ade062e58018021dd6e8259ef1
Lucas Miller <lucas.miller@shopify.com>
2018-05-16 18:27:09 -0700

    Added css for dark mode that makes the sidebar also dark mode

--- js/editor.js
+++ js/editor.js
@@ -28,6 +28,22 @@ function _applyCss() {
           $('body').addClass('theme-editor--dark');
           addCSSRule(document.styleSheets[cssFile], '.asset-search', 'background-color: inherit !important', 0);
           addCSSRule(document.styleSheets[cssFile], '.CodeMirror-lines', 'background-color: #0e1216 !important', 0);
+          $('.ui-nav__link--parent.ui-nav__link--parent')
+          .css('color', '#c4cdd5')
+          .hover(function() {
+              $(this).css('color', '#5c6ac4')
+          }, function(){
+            $(this).css('color', '#c4cdd5')
+          });
+          $('.ui-nav__link--parent.ui-nav__link--parent.ui-button--link')
+          .css('color', '#c4cdd5')
+          .hover(function() {
+              $(this).css('color', '#5c6ac4')
+          }, function(){
+            $(this).css('color', '#c4cdd5')
+          });
+          $('.ui-app-frame__aside').css('background-color', '#0e1216');
+
       }
 
       //Run through the custom colours and add css overrides to the stylesheet
